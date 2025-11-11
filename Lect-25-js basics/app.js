5c391d47c969b42b82b0e4ada6e88f70526f7539
Vikas Patel <vikaspatel@Vikass-MacBook-Air.local>
2025-11-11 13:46:21 +0530

    js basics

---
+++ Lect-25-js basics/app.js
@@ -0,0 +1,45 @@
+// console.log("alpha batch mirai !");
+
+// let n=29;
+// n=n/10;
+// n=parseInt(n/10);
+// console.log(n);
+
+
+// let a=10,b=20,c=30;
+
+
+// let a=90;
+
+
+
+// function sum(a,b){
+//     let vikas="vikas patel";
+//     console.log("inside function")
+//     console.log(a+b);
+//     console.log(vikas);
+    
+//     return a+b+1;
+// }
+
+// console.log(vikas);
+
+
+// let ans=sum(2,4);
+// let ans=7;
+// console.log(ans);
+
+// var a;
+
+if(true){
+var a=90;
+    a=a+1;
+    console.log(a);
+    
+
+}
+console.log(a);
+
+
+// console.log(a);
+// var a=90;
---
+++ Lect-25-js basics/script.js
@@ -0,0 +1,57 @@
+// ;
+// Hoisting in JavaScript is a behavior where variable and function declarations
+//  are conceptually moved to the top of their containing scope during the 
+//  compilation phase, before the code is executed.This means that you can 
+//  use a variable or call a function before it is explicitly declared in
+//   your code. 
+
+// console.log(a);
+// var a;
+// a=10;
+// console.log(a);
+
+
+
+
+// if(true){
+// let a=90;
+//     a=a+1;
+//     console.log(a);
+    
+
+// }
+// console.log(a);
+
+
+// for(let i=0;i<10;i++){
+//     console.log("hello");
+    
+// }
+// i++;
+// console.log(i);
+
+
+// const a=90;
+// a++;
+// console.log(a);
+
+
+
+
+// let a="vikas";
+// let b="patel";
+// let sum=a*b;
+// console.log(sum);
+
+
+
+
+// console.log(a);
+// let a;
+
+let a;
+console.log(a);
+
+
+
+
